6f73d97396b9268d9125a4ceb3da141130afbaf5
Alex <baklan4ik1993@gmail.com>
2024-07-11 19:21:20 +0200

    navbar

---
+++ src/components/nav/DropdownProfile.tsx
@@ -0,0 +1,47 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+  
+type User = {
+    username: string;
+    avatar: string;
+}
+
+const DropdownProfile = ({username, avatar}: User) => {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className='h-8 w-8 cursor-pointer'>
+              <AvatarImage src={avatar} />
+              <AvatarFallback>{username.substring(0,1)}</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>{username}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Billing
+            </DropdownMenuItem>
+            
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <p className=" text-red-600">Log out</p>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
+
+export default DropdownProfile
---
+++ src/components/nav/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+import { Button } from '../ui/button';
+import DropdownProfile from './DropdownProfile';
+import NavigationWorkspaces from './NavigationWorkspaces';
+import { motion } from 'framer-motion';
+
+const workspaces = [
+  { id: "1", title: 'Project 1', dateOfChange: new Date() },
+  { id: "2", title: 'Project 2', dateOfChange: new Date() },
+];
+
+const user = {
+  username: "alex",
+  avatar: "https://github.com/shadcn.png",
+}
+
+const Nav = () => {
+
+  if (user == null) return null;
+  return (
+    <motion.nav initial={{opacity: 0}} animate={{opacity:1}} transition={{delay:1.3, duration: 1}} className='h-10 flex items-center justify-between px-3'>
+      <div className='flex gap-3 items-center'>
+        <Button size='sm' variant="link">Home</Button>
+        <NavigationWorkspaces workspaces={workspaces} />
+      </div>
+      <div className='flex gap-3'>
+            <DropdownProfile username = {user.username} avatar={user.avatar}/>
+        <Button size={"sm"}>Upgrade</Button>
+      </div>
+    </motion.nav>
+  );
+}
+
+
+
+export default Nav;
